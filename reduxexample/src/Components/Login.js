f4ffc6de5c8243b0b12994bb913bd9fa511e746e
Fidela1 <fidelatuyizere4@gmail.com>
2023-03-22 22:58:23 +0200

    create login with redux

---
+++ reduxexample/src/Components/Login.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react'
+
+const login = () => {
+    // const [name, setName] = useState("");
+    // const [name, setName] = useState("");
+    // const [name, setName] = useState("");
+
+  return (
+    <div className='login'>
+        <form className='login_form'>
+<h1> Login</h1>
+<input type='Name' placeholder='name'/>
+<input type='Email' placeholder='email'/>
+<input type='Password' placeholder='password'/>
+        </form>
+      
+    </div>
+  )
+}
+
+export default login
