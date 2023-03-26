fc51a959d42808cf02e673d2b446ad367e115a40
Fidela1 <fidelatuyizere4@gmail.com>
2023-03-26 22:56:54 +0200

    Create login page

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,4 +1,7 @@
-import Login from './Components/Login'
+import React from 'react';
+import Login from './Components/Login';
+import './App.css'
+
 function App() {
   return (
     <div>
--- reduxexample/src/Components/Login.js
+++ reduxexample/src/Components/Login.js
@@ -1,17 +1,29 @@
-// import React, { useState } from 'react'
-
+import React, { useState } from 'react'
+import './Login.css'
 const login = () => {
-    // const [name, setName] = useState("");
-    // const [name, setName] = useState("");
-    // const [name, setName] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
   return (
     <div className='login'>
         <form className='login_form'>
 <h1> Login</h1>
-<input type='Name' placeholder='name'/>
-<input type='Email' placeholder='email'/>
-<input type='Password' placeholder='password'/>
+<input type='Name'
+ placeholder='name'
+  value={name} 
+  onChange = {(e) => setName(e.target.value)}/>
+
+<input type='Email' 
+placeholder='email' 
+value={email}
+onChange = {(e) => setEmail(e.target.value)}/>
+<input type='Password' 
+placeholder='password' 
+value={password}
+onChange = {(e) => setPassword(e.target.value)}/>
+
+<button type='submit' className='submit_btn'>submit</button>
         </form>
       
     </div>
