12a7bca3b63bf14dbceaa8dec783bf7356d952f5
Fidela1 <fidelatuyizere4@gmail.com>
2023-03-27 14:45:41 +0200

    create logout page

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './Components/Login';
+import Logout from './Components/Logout';
 import './App.css'
 
 function App() {
   return (
     <div>
   <Login />
+  <Logout />
     </div>
   )
 }
