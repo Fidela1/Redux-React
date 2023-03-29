57fd662f178417eb3a11ad7628f0744a79896b9f
Fidela1 <fidelatuyizere4@gmail.com>
2023-03-29 10:03:56 +0200

    login and logout with redux

--- reduxexample/src/Components/Login.js
+++ reduxexample/src/Components/Login.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { login } from '../features/userSlice';
 import './Login.css'
-const login = () => {
+const Login = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      dispatch(
+        login({
+          name:name,
+          email:email,
+          password:password,
+          loggedIn:true,
+})
+)
+      
+    }
   return (
     <div className='login'>
-        <form className='login_form'>
+        <form className='login_form' onSubmit={(e) => handleSubmit(e)}>
 <h1> Login</h1>
 <input type='Name'
  placeholder='name'
@@ -30,4 +46,4 @@ onChange = {(e) => setPassword(e.target.value)}/>
   )
 }
 
-export default login
+export default Login
